0c0a2ddba56fb0c0e9c3203af29a2eea1051fbbe
Said-Duran <162299865+Said-Duran@users.noreply.github.com>
2026-03-31 14:55:35 +0200

    Correct import and test for gcdBruteForce
    Fix import statement and test function name.

---
+++ gcd_test.ts
@@ -0,0 +1,9 @@
+importt { gcdBruteForce } from "./gcd.ts";
+
+Deno.test("ggT von 1 und 1 ist 1", () => {
+  const result = gdcsBruteForce(1,1);
+
+  if (result !== 1) {
+    throw new Error("Falsch");
+  }
+});
