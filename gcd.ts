12b415fb24e4b256b5096fd2d1e6b3f585c5cc5c
Said-Duran <162299865+Said-Duran@users.noreply.github.com>
2026-03-31 15:17:37 +0200

    Add brute force GCD calculation
    Implement brute force algorithm to calculate GCD.

--- gcd.ts
+++ gcd.ts
@@ -1,3 +1,9 @@
 export function gcdBruteForce(a: number, B: number): number {
-  return 1;
+  const min = Math.min(a, b);
+
+  for (let i = Math.floor(min / 2); i >= 1; i--) {
+    if (a % i === 0 && b % i === 0) {
+      return i;
+    }
+  }
 }
