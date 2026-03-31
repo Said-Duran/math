4bc3ce7daa6b21bac5d5e16214c1f71a68592780
Said-Duran <162299865+Said-Duran@users.noreply.github.com>
2026-03-31 14:59:30 +0200

    Add test for gcdBruteForce function

--- gcd_test.ts
+++ gcd_test.ts
@@ -7,3 +7,11 @@ Deno.test("ggT von 1 und 1 ist 1", () => {
     throw new Error("Falsch");
   }
 });
+
+Deno.test("ggT von 6 und 3 ist 3", () => {
+  const result = gcdBruteForce(6, 3);
+
+          if (result !== 3) 
+    throw new Error("Falsch");
+  }
+});
