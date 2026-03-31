d826582f51ecaccb85b6ede8177a56549383bcd7
Said-Duran <162299865+Said-Duran@users.noreply.github.com>
2026-03-31 15:24:07 +0200

    Add additional tests for gcdBruteForce function

--- gcd_test.ts
+++ gcd_test.ts
@@ -15,3 +15,22 @@ Deno.test("ggT von 6 und 3 ist 3", () => {
     throw new Error("Falsch");
   }
 });
+
+Deno.test("ggT von 8 und 12 ist 4", () => {
+  if (gcdBruteForce(8, 12) !== 4) {
+    throw new Error("Falsch");
+  }
+});
+
+Deno.test("ggT von 7 und 5 ist 1", () => {
+  if (gcdBruteForce(7, 5) !== 1) {
+    throw new Error("Falsch");
+  }
+});
+
+Deno.test("ggT von gleichen Zahlen", () => {
+  if (gcdBruteForce(5, 5) !== 5) {
+    throw new Error("Falsch");
+  }
+});
+
